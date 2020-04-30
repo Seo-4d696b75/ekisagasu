adc22fa029e3d8f8533f72b0ba917059fe5a09bb
Seo-4d696b75 <s.kaoru509@gmail.com>
2020-04-30 22:03:50 +0900

    [update] web meta data
    add icon image, page description, etc.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,13 @@ import React from "react";
 
 export default class Footer extends React.Component {
 
-	onTextChanged(e) {
-	}
-	onSynClicked() {
-	}
 
 	render() {
 		return (
 			<div className='Map-footer'>
 				<span>
 
-					<input onChange={this.onTextChanged}></input>
-					<button onClick={this.onSynClicked}>Sync</button>
-					This pages is made with React.js
+					This page was made with React.js
 				</span>
 			</div>
 		);
