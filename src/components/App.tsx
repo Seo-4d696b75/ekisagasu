aaa41854d96bbe08c9bd1a425fbcd9e9a8140ea6
senda_k <s.kaoru509@gmail.com>
2022-05-07 16:33:51 +0900

    修正：app hook化

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import './App.css';
-import Header from './Header'
-import Map from './map/Map'
-import Help from './Help'
+import { FC } from 'react';
+import { Provider } from "react-redux";
 import { HashRouter, Route } from 'react-router-dom';
-import { Provider } from "react-redux"
-import { store } from "../script/store"
+import { store } from "../script/store";
+import './App.css';
+import Header from './Header';
+import Help from './Help';
+import Map from './map/Map';
 
-export default class APP extends React.Component {
-
-	render() {
-		return (
-			<div className="App">
-				<HashRouter basename='/'>
-					<Route exact path='/' render={(props) => {
-						return (
-							<div>
-								<Provider store={store}>
-									<Header></Header>
-									<Map></Map>
-								</Provider>
-							</div>
-						)
-					}}></Route>
-					<Route path='/help' component={Help}></Route>
-				</HashRouter>
-			</div>
-		)
-	}
+const APP: FC = () => {
+  return (
+    <div className="App">
+      <HashRouter basename='/'>
+        <Route exact path='/'>
+          <div>
+            <Provider store={store}>
+              <Header></Header>
+              <Map></Map>
+            </Provider>
+          </div>
+        </Route>
+        <Route path='/help' component={Help}></Route>
+      </HashRouter>
+    </div>
+  )
 }
+
+export default APP
