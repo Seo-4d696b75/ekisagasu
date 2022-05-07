859ec36d6f76d2dd7573dd2dfab9ebe90b4c1630
senda_k <s.kaoru509@gmail.com>
2022-05-07 16:27:23 +0900

    修正：mapリファクタ

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,29 +1,22 @@
 import { CircularProgress } from "@material-ui/core"
-import { Circle, GoogleAPI, GoogleApiWrapper, IMapProps, Map, Marker, Polygon, Polyline } from "google-maps-react"
-import qs from "query-string"
+import { Circle, GoogleAPI, GoogleApiWrapper, Map, Marker, Polygon, Polyline } from "google-maps-react"
 import { FC, useEffect, useMemo, useRef, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useLocation } from "react-router-dom"
+import { useSelector } from "react-redux"
 import { CSSTransition } from "react-transition-group"
 import pin_location from "../../img/map_pin.svg"
 import pin_station from "../../img/map_pin_station.svg"
-import * as action from "../../script/actions_"
-import { Line } from "../../script/Line"
-import { LatLng } from "../../script/location"
 import { RootState } from "../../script/mapState"
-import { Station } from "../../script/Station"
 import StationService from "../../script/StationService"
-import { AppDispatch } from "../../script/store_"
-import { get_bounds, get_zoom_property, parseQueryBoolean, PolylineProps, RectBounds } from "../../script/Utils"
 import { CurrentPosDialog } from "../CurrentPosDialog"
-import { useEventEffect, useRefCallback } from "../hooks"
+import { useEventEffect } from "../hooks"
 import { LineDialog } from "../LineDialog"
 import { CurrentPosDialogProps, DialogType, isInfoDialog, isStationDialog, LineDialogProps, NavState, NavType, SelectPosDialogProps, StationDialogProps } from "../MapNavState"
 import { CurrentPosIcon } from "../MapSections"
 import { StationDialog } from "../StationDialog"
-import { useServiceCallback } from "./serviceHook"
 import "./Map.css"
-import { useHighVoronoi } from "./voronoiHook"
+import { useMapCallback } from "./mapEventHook"
+import { useMapOperator } from "./mapHook"
+import { useServiceCallback } from "./serviceHook"
 
 const VORONOI_COLOR = [
   "#0000FF",
@@ -31,23 +24,8 @@ const VORONOI_COLOR = [
   "#FF0000",
   "#CCCC00"
 ]
-
-const ZOOM_TH_VORONOI = 10
-const ZOOM_TH_PIN = 12
-const VORONOI_SIZE_TH = 500
-
 interface MapProps {
   google: GoogleAPI
-}
-
-function getUIEvent(clickEvent: any): UIEvent {
-  // googlemap onClick などのコールバック関数に渡させるイベントオブジェクトの中にあるUIEventを抽出
-  // property名が謎
-  // スマホではTouchEvent, マウスでは MouseEvent
-  for (var p in clickEvent) {
-    if (clickEvent[p] instanceof UIEvent) return clickEvent[p]
-  }
-  throw Error("UIEvent not found")
 }
 
 const MapContainer: FC<MapProps> = ({ google: googleAPI }) => {
@@ -63,20 +41,52 @@ const MapContainer: FC<MapProps> = ({ google: googleAPI }) => {
     stations: voronoi,
   } = useSelector((state: RootState) => state.mapState)
 
-  const [hideVoronoi, setHideVoronoi] = useState(false)
-  const [hideStationPin, setHideStationPin] = useState(false)
   const [screenWide, setScreenWide] = useState(false)
 
   const googleMapRef = useRef<google.maps.Map | null>(null)
   const mapElementRef = useRef<HTMLDivElement>(null)
-  const uiEventRef = useRef<UIEvent | null>(null)
 
-  const dispatch = useDispatch<AppDispatch>()
-
+  // callbacks registered to StationService
   const {
     onGeolocationPositinoChanged,
     onStationLoaded,
   } = useServiceCallback()
+
+  // functions operating the map and its state variables
+  const {
+    highVoronoi,
+    workerRunning,
+    hideStationPin,
+    hideVoronoi,
+    showStation,
+    showLine,
+    moveToCurrentPosition,
+    setCenterCurrentPosition,
+    showRadarVoronoi,
+    showPolyline,
+    updateBounds,
+    closeDialog,
+    focusAt,
+    focusAtNearestStation,
+    requestCurrentPosition,
+  } = useMapOperator(googleMapRef, mapElementRef)
+
+  // callbacks listening to map events
+  const {
+    onMapClicked,
+    onMapRightClicked,
+    onMapDragStart,
+    onMapIdle,
+    onMapReady,
+  } = useMapCallback(screenWide, googleMapRef, {
+    focusAt,
+    focusAtNearestStation,
+    closeDialog,
+    updateBounds,
+    showPolyline,
+    showRadarVoronoi,
+    setCenterCurrentPosition
+  })
 
   useEffect(() => {
     // componentDidMount
@@ -98,22 +108,6 @@ const MapContainer: FC<MapProps> = ({ google: googleAPI }) => {
     }
   }, [onGeolocationPositinoChanged, onStationLoaded])
 
-  const showStation = (station: Station) => {
-    dispatch(action.requestShowStation(station))
-  }
-
-  const showLine = (line: Line) => {
-    dispatch(action.requestShowLine(line))
-  }
-
-  const moveToCurrentPosition = (pos: google.maps.LatLng | null) => {
-    dispatch(action.setNavStateIdle())
-    const map = googleMapRef.current
-    if (pos && map) {
-      map.panTo(pos)
-    }
-  }
-
   useEventEffect(focus, pos => {
     const map = googleMapRef.current
     if (map) {
@@ -131,223 +125,9 @@ const MapContainer: FC<MapProps> = ({ google: googleAPI }) => {
     }
   })
 
-  const setCenterCurrentPosition = (map: google.maps.Map) => {
-    // no move animation
-    StationService.get_current_position().then(pos => {
-      let latlng = {
-        lat: pos.coords.latitude,
-        lng: pos.coords.longitude
-      }
-      map.setCenter(latlng)
-      dispatch(action.setCurrentLocation(pos))
-    }).catch(err => {
-      console.log(err)
-      alert("現在位置を利用できません. ブラウザから位置情報へのアクセスを許可してください.")
-    })
-  }
-
-  const { run: runHighVoronoi, cancel: cancelHighVoronoi, highVoronoi, workerRunning } = useHighVoronoi(radarK, {
-    onStart: (_) => dispatch(action.requestShowHighVoronoi()),
-    onComplete: (station, list) => {
-      const map = googleMapRef.current
-      const mapElement = mapElementRef.current
-      if (map && mapElement) {
-        var rect = mapElement.getBoundingClientRect()
-        var bounds = get_bounds(list[radarK - 1])
-        var props = get_zoom_property(bounds, rect.width, rect.height, ZOOM_TH_VORONOI, station.position, 100)
-        map.panTo(props.center)
-        map.setZoom(props.zoom)
-      }
-    },
-    onError: (_) => dispatch(action.setNavStateIdle()),
-  })
-
-  const showRadarVoronoi = (station: Station) => {
-    if (!isStationDialog(nav)) return
-    if (nav.data.showHighVoronoi) {
-      dispatch(action.setNavStateIdle())
-      return
-    }
-    runHighVoronoi(station)
-  }
-
-  const showPolyline = useRefCallback((line: Line) => {
-    const map = googleMapRef.current
-    if (!line.detail || !map) return
-    if (nav.type !== NavType.DIALOG_LINE) return
-    if (nav.data.showPolyline) return
-    let polyline: PolylineProps[] = []
-    let bounds: RectBounds
-    if (line.detail.polylines) {
-      polyline = line.detail.polylines
-      bounds = line.detail
-    } else {
-      let stations = line.detail.stations
-      let data = get_bounds(line.detail.stations)
-      polyline = [{
-        points: stations.map(s => s.position),
-        start: stations[0].name,
-        end: stations[stations.length - 1].name,
-      }]
-      bounds = data
-    }
-    dispatch(action.requestShowPolyline({
-      dialog: nav.data.dialog,
-      polylines: polyline,
-      stations: line.detail.stations.map(s => s.position),
-    }))
-    const mapElement = mapElementRef.current
-    if (mapElement) {
-      var rect = mapElement.getBoundingClientRect()
-      var props = get_zoom_property(bounds, rect.width, rect.height)
-
-      map.panTo(props.center)
-      map.setZoom(props.zoom)
-      console.log('zoom to', props, line)
-    }
-  })
-
-  const location = useLocation()
-
-  const onMapReady = async (props?: IMapProps, map?: google.maps.Map, event?: any) => {
-    console.log("map ready", props)
-    if (map) {
-
-      map.addListener("mousedown", event => uiEventRef.current = getUIEvent(event))
-      googleMapRef.current = map
-      map.setOptions({
-        // this option can not be set via props in google-maps-react
-        mapTypeControlOptions: {
-          position: google.maps.ControlPosition.TOP_RIGHT,
-          style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-        }
-      })
-      dispatch(action.setNavStateIdle())
-
-      const s = await StationService.initialize()
-      // parse query actions
-      const query = qs.parse(location.search)
-      if (typeof query.line == 'string') {
-        console.log('query: line', query.line)
-        var line = s.get_line_by_id(query.line)
-        if (line) {
-          try {
-            let result = await dispatch(action.requestShowLine(line)).unwrap()
-            showPolyline(result.line)
-          } catch (e) {
-            console.warn("fail to show line details. query:", query.line, e)
-          }
-          return
-        }
-      }
-      if (typeof query.station == 'string') {
-        console.log('query: station', query.station)
-        try {
-          let result = await dispatch(action.requestShowStationPromise(
-            s.get_station_by_id(query.station)
-          )).unwrap()
-          if (typeof query.voronoi == 'string') {
-            const str = query.voronoi.toLowerCase().trim()
-            if (parseQueryBoolean(str)) {
-              showRadarVoronoi(result.station)
-            }
-          }
-        } catch (e) {
-          console.warn("fail to show station, query:", query.station, e)
-          setCenterCurrentPosition(map)
-        }
-        return
-      }
-      if (typeof query.mylocation == 'string') {
-        console.log('query: location', query.mylocation)
-        if (parseQueryBoolean(query.mylocation)) {
-          dispatch(action.setWatchCurrentLocation(true))
-        }
-      }
-      // if no query, set map center current position
-      setCenterCurrentPosition(map)
-
-    }
-  }
-
-  const updateBounds = (map: google.maps.Map) => {
-    const bounds = map.getBounds()
-    if (!bounds) return
-    const zoom = map.getZoom()
-    setHideVoronoi(zoom < ZOOM_TH_VORONOI)
-    setHideStationPin(zoom < ZOOM_TH_PIN)
-    if (zoom >= ZOOM_TH_VORONOI) {
-      var ne = bounds.getNorthEast()
-      var sw = bounds.getSouthWest()
-      var margin = Math.max(ne.lat() - sw.lat(), ne.lng() - sw.lng()) * 0.5
-      var rect = {
-        south: sw.lat() - margin,
-        north: ne.lat() + margin,
-        west: sw.lng() - margin,
-        east: ne.lng() + margin,
-      }
-      StationService.update_rect(rect, VORONOI_SIZE_TH)
-    }
-  }
-
-  const onInfoDialogClosed = () => {
-    // if any worker is running, terminate it
-    cancelHighVoronoi()
-    dispatch(action.setNavStateIdle())
-  }
-
-  const focusAt = (pos: LatLng) => {
-    if (!StationService.initialized) return
-    if (isStationDialog(nav) && nav.data.showHighVoronoi) return
-    dispatch(action.requestShowSelectedPosition(pos))
-  }
-
-  const focusAtNearestStation = (pos: LatLng) => {
-    if (!StationService.initialized) return
-    if (isStationDialog(nav) && nav.data.showHighVoronoi) return
-    StationService.update_location(pos, radarK, 0).then(s => {
-      console.log("update location", s)
-      if (s) dispatch(action.requestShowStation(s))
-    })
-  }
-
-  const onMapClicked = (props?: IMapProps, map?: google.maps.Map, event?: any) => {
-    const pos = {
-      lat: event.latLng.lat(),
-      lng: event.latLng.lng()
-    }
-    const previous = uiEventRef.current
-    if (previous && getUIEvent(event).timeStamp - previous.timeStamp > 300) {
-      console.log("map long clicked", pos, event)
-      focusAt(pos)
-    } else {
-      console.log("map clicked", event)
-      focusAtNearestStation(pos)
-    }
-  }
-
-  const onMapRightClicked = (props?: IMapProps, map?: google.maps.Map, event?: any) => {
-    const pos = {
-      lat: event.latLng.lat(),
-      lng: event.latLng.lng()
-    }
-    //console.log("right click", pos, event)
-    focusAt(pos)
-  }
-
-  const onMapDragStart = (props?: IMapProps, map?: google.maps.Map) => {
-    if (isStationDialog(nav) && nav.data.showHighVoronoi) return
-    if (nav.type === NavType.DIALOG_LINE && nav.data.showPolyline) return
-    if (!screenWide) {
-      onInfoDialogClosed()
-    }
-  }
-
-  const onMapIdle = (props?: IMapProps, map?: google.maps.Map, event?: any) => {
-    if (StationService.initialized && map) {
-      updateBounds(map)
-    }
-  }
+  /* ===============================
+   render section below
+  ================================ */
 
   const currentPosition = currentLocation?.position
   const currentPositionMarker = useMemo(() => {
@@ -555,7 +335,7 @@ const MapContainer: FC<MapProps> = ({ google: googleAPI }) => {
             <LineDialog
               info={infoDialogProps}
               onStationSelected={showStation}
-              onClosed={onInfoDialogClosed}
+              onClosed={closeDialog}
               onShowPolyline={showPolyline} />
           ) : (infoDialogProps?.type === DialogType.STATION ||
             infoDialogProps?.type === DialogType.SELECT_POSITION) ? (
@@ -563,7 +343,7 @@ const MapContainer: FC<MapProps> = ({ google: googleAPI }) => {
               info={infoDialogProps}
               onStationSelected={showStation}
               onLineSelected={showLine}
-              onClosed={onInfoDialogClosed}
+              onClosed={closeDialog}
               onShowVoronoi={showRadarVoronoi} />
           ) : null}
           {progressDialog}
@@ -590,19 +370,6 @@ const MapContainer: FC<MapProps> = ({ google: googleAPI }) => {
       </div>
     </CSSTransition>
   )
-
-  const onCurrentPosRequested = () => {
-    if (showCurrentPosition) {
-      if (currentPosition) {
-        moveToCurrentPosition(new google.maps.LatLng(currentPosition.lat, currentPosition.lng))
-      }
-    } else {
-      onInfoDialogClosed()
-      StationService.get_current_position().then(pos => {
-        moveToCurrentPosition(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude))
-      })
-    }
-  }
 
   return (
     <div className='Map-container'>
@@ -638,7 +405,7 @@ const MapContainer: FC<MapProps> = ({ google: googleAPI }) => {
 
         {InfoDialog}
         {currentPosDialog}
-        <CurrentPosIcon onClick={onCurrentPosRequested} />
+        <CurrentPosIcon onClick={requestCurrentPosition} />
       </div>
     </div>
   )
