20ee5651cfbdd67ef109da3f025c74d7fb90af45
senda_k <s.kaoru509@gmail.com>
2022-05-13 00:13:12 +0900

    追加：RTL

--- jest.config.js
+++ jest.config.js
@@ -12,5 +12,9 @@ module.exports = {
     "ts-jest": {
       tsconfig: "tsconfig.test.json"
     }
-  }
+  },
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.ts",
+    "\\.(css|scss)$": "<rootDir>/__mocks__/styleMock.ts",
+  },
 }
