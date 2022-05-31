28d6fb1ed417a21f95794efc4d1f97c21f85cb0c
senda_k <s.kaoru509@gmail.com>
2022-05-31 22:51:47 +0900

    追加：API URL

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -7,6 +7,7 @@ declare global {
       REACT_APP_RADAR_MIN: number
       REACT_APP_RADAR_MAX: number
       REACT_APP_DATA_BASE_URL: string
+      REACT_APP_DATA_EXTRA_BASE_URL: string
       REACT_APP_PREFECTURE_URL: string
       REACT_APP_STATION_API_URL: string
       NODE_ENV: 'development' | 'production'
