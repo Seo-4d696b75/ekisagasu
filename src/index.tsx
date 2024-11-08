d82e7cd6added77bacdcaf9e83a5fe8ad604fb0c
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-11-09 01:48:19 +0900

    fix: react-dom render warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,11 @@
-import ReactDOM from 'react-dom';
-import React from "react"
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <App></App>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App></App>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
