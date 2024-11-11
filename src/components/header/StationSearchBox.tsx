32525e383a19d9fcb8a2f70e15707a376fae1bc0
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-11-11 22:16:30 +0900

    fix: import Material UI package

--- src/components/header/StationSearchBox.tsx
+++ src/components/header/StationSearchBox.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress } from "@material-ui/core"
+import { CircularProgress } from "@mui/material"
 import axios from "axios"
 import { FC, useCallback, useMemo, useRef, useState } from "react"
 import Autosuggest from 'react-autosuggest'
