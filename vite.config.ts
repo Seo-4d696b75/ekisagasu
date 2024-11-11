d9a406f781878b6dcb871426ad8a3a193cf23e84
Seo-4d696b75 <s.kaoru509@gmail.com>
2024-11-11 22:46:01 +0900

    feat: vite.config.ts added

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// copy from https://zenn.dev/xronotech/articles/11e671bf0315e7
+export default defineConfig({
+  server: {
+    open: true, // 自動でブラウザを開く
+    port: 3000, // ここでポート番号を指定します
+  },
+  build: {
+    outDir: 'build', // ビルドの出力先ディレクトリ
+  },
+  plugins: [react()],
+});
